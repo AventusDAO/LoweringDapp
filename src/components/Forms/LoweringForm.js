58eff2ac013e132786aa3e2672291c56ee5a0f78
fluorostani <glyn.dimond@artos.io>
2023-01-31 13:50:07 +0000

    more tweaks

--- src/components/Forms/LoweringForm.js
+++ src/components/Forms/LoweringForm.js
@@ -18,7 +18,7 @@ function LoweringForm() {
     }
 
     let title = "Initiate Lower";
-    let description = "Migrate Your Tokens on Aventus to Ethereum";
+    let description = "Lower your tokens from the AvN to Ethereum";
     const tokenTabs = ["AVT", "ERC20", "ERC777", "ETH"];
     const { theme } = useContext(ThemeContext);
 
@@ -48,7 +48,7 @@ function LoweringForm() {
                                             role="presentation"
                                         >
                                             <button
-                                                className={`nav-link 
+                                                className={`nav-link
                                                     ${
                                                         index === 0
                                                             ? "active"
