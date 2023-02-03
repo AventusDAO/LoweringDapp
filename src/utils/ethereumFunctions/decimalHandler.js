0fafe2bc3b2fc2b6d61ff64fccbd1da2965056dc
fluorostani <glyn.dimond@artos.io>
2023-02-03 16:46:05 +0000

    Hotfix for decimal handler

--- src/utils/ethereumFunctions/decimalHandler.js
+++ src/utils/ethereumFunctions/decimalHandler.js
@@ -29,7 +29,7 @@ async function fullDecimalAmount(amount, token, isERC777) {
     }
 
     let [intAmount, decAmount] = amount.toString().split(".");
-    decAmount = (decAmount.length > decimals) ? decAmount.substring(0, decimals) : decAmount;
+    if (decAmount && decAmount.length > decimals) decAmount = decAmount.substring(0, decimals);
     intAmount = new BN(intAmount).mul(new BN(10).pow(new BN(decimals)));
 
     decAmount = decAmount
