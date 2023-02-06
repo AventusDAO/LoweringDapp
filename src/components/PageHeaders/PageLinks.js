9a6bd269e58bb9501f19176d3bab9d9fd0a75966
Chinazaekpere Ngubo <emmanuel.ngubo@aventus.io>
2023-02-06 13:23:24 +0000

    change page links

--- src/components/PageHeaders/PageLinks.js
+++ src/components/PageHeaders/PageLinks.js
@@ -5,13 +5,13 @@ export const PageLinks = () => {
     return (
         <div className="mx-auto headerLinks">
             <Link className="pageLinks" to="/">
-                Lower{" "}
+                Step 1{" "}
+            </Link>
+            <Link className="pageLinks" to="/claim">
+                Step 2{" "}
             </Link>
             <Link className="pageLinks" to="/balance">
                 Balance{" "}
-            </Link>
-            <Link className="pageLinks" to="/claim">
-                Claim{" "}
             </Link>
         </div>
     );
