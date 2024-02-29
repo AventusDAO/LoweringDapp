f033e8ecd86e5bd056f9d07fa5c36157aa0ecdd3
IvanCholakov <ivan.cholakov@aventus.io>
2024-02-29 14:39:13 +0200

    fix: obfuscated lowering dapp

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,11 @@
 const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator');
+
 
 module.exports = function override(config, env) {
-    return {
+    let overrideConfig = {
         ...config,
         resolve: {
             ...config.resolve,
@@ -56,4 +60,60 @@ module.exports = function override(config, env) {
             ],
         },
     };
+    if (env == "production") {
+        overrideConfig = {
+            ...overrideConfig,
+            plugins:[
+                ...overrideConfig.plugins,
+                new WebpackObfuscator ({
+                    rotateStringArray: true
+                }, ['excluded_bundle_name.js'])
+            ],
+            module: {
+                ...config.module,
+            },
+            devtool: false,
+            optimization: {
+                minimize: true,
+                minimizer: [
+                  // This is only used in production mode
+                  new TerserPlugin({
+                    terserOptions: {
+                      parse: {
+                        ecma: 8,
+                      },
+                      compress: {
+                        ecma: 5,
+                        warnings: false,
+                        comparisons: false,
+                        inline: 2,
+                      },
+                      mangle: {
+                        safari10: true,
+                      },
+                      // Added for profiling in devtools
+                      keep_classnames: false,
+                      keep_fnames: false,
+                      output: {
+                        ecma: 5,
+                        comments: false,
+                        // Turned on because emoji and regex is not minified properly using default
+                        // https://github.com/facebook/create-react-app/issues/2488
+                        ascii_only: true,
+                      },
+                    },
+                  }),
+                  // This is only used in production mode
+                  new CssMinimizerPlugin(),
+                ],
+              },
+           
+        }
+
+        
+        
+        console.log("HELP !!!!!!!", overrideConfig.optimization)
+        return overrideConfig;
+    }
+    return overrideConfig
 };
