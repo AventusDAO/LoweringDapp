5f68811f97e52b8ab7b2553656a9b28c4cf3cd7f
Ivan Cholakov <icholakov1@gmail.com>
2024-03-01 13:26:45 +0200

    SYS-3917: obfuscated and minified lowering dapp (#33)

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,11 @@
 const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator');
+
 
 module.exports = function override(config, env) {
-    return {
+    let overrideConfig = {
         ...config,
         resolve: {
             ...config.resolve,
@@ -56,4 +60,51 @@ module.exports = function override(config, env) {
             ],
         },
     };
+    if (env == "production") {
+        overrideConfig = {
+            ...overrideConfig,
+            plugins:[
+                ...overrideConfig.plugins,
+                new WebpackObfuscator ({
+                    rotateStringArray: true
+                }, ['excluded_bundle_name.js'])
+            ],
+            module: {
+                ...config.module,
+            },
+            devtool: false,
+            optimization: {
+                minimize: true,
+                minimizer: [
+                  new TerserPlugin({
+                    terserOptions: {
+                      parse: {
+                        ecma: 8,
+                      },
+                      compress: {
+                        ecma: 5,
+                        warnings: false,
+                        comparisons: false,
+                        inline: 2,
+                      },
+                      mangle: {
+                        safari10: true,
+                      },
+                      keep_classnames: false,
+                      keep_fnames: false,
+                      output: {
+                        ecma: 5,
+                        comments: false,
+                        ascii_only: true,
+                      },
+                    },
+                  }),
+                  new CssMinimizerPlugin(),
+                ],
+              },
+           
+        }
+        return overrideConfig;
+    }
+    return overrideConfig
 };
